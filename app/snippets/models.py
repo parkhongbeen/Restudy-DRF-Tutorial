4cadef70d0cd2281591e5164db577225f83145df
parkhongbeen <pack122@naver.com>
2020-02-14 19:07:04 +0900

    snippetserializer를 직접 구현함

--- app/snippets/models.py
+++ app/snippets/models.py
@@ -6,6 +6,7 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
+
 class Snippet(models.Model):
     class Snippet(models.Model):
         # DB index설정 (Field.db_index)
