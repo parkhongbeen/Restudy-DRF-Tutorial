c42fbb6419a3ceb37848b0084206fbef62ab69f3
parkhongbeen <pack122@naver.com>
2020-02-17 16:53:25 +0900

    api.py 패키지화

--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from . import views, apis
+
+from . import apis
+from .apis import api_view
 
 app_name = 'snippets'
 urlpatterns = [
     # path('snippets/', views.snippet_list),
     # path('snippets/<int:pk>/', views.snippet_detail),
-    path('snippets/', apis.SnippetListCreateAPIView.as_view()),
-    path('snippets/<int:pk>', apis.SnippetRetrieveUpdateDestroyAPIView.as_view()),
+    path('snippets/', api_view.SnippetListCreateAPIView.as_view()),
+    path('snippets/<int:pk>', api_view.SnippetRetrieveUpdateDestroyAPIView.as_view()),
 ]
