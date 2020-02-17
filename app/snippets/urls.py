0d41baf4653d376920dbb9003819b63b92e3988b
parkhongbeen <pack122@naver.com>
2020-02-17 16:05:26 +0900

    snippet_detail의 url구현

--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'snippets'
 urlpatterns = [
-    path('snippets/', views.snippet_list)
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
 ]
