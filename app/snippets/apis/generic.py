086386b32c43e0f0d94354169ea50f010f4c2354
parkhongbeen <pack122@naver.com>
2020-02-17 17:13:50 +0900

    Generic Class-based APIView를 사용

---
+++ app/snippets/apis/generic.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+
+from ..models import Snippet
+from ..serializers import SnippetSerializer
+
+
+class SnippetListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from . import apis
-from .apis import api_view, mixin
+from .apis import api_view, mixin, generic
 
 app_name = 'snippets'
 urlpatterns = [
     # path('snippets/', views.snippet_list),
     # path('snippets/<int:pk>/', views.snippet_detail),
-    path('snippets/', mixin.SnippetListCreateAPIView.as_view()),
-    path('snippets/<int:pk>', mixin.SnippetRetrieveUpdateDestroyAPIView.as_view()),
+    path('snippets/', generic.SnippetListCreateAPIView.as_view()),
+    path('snippets/<int:pk>', generic.SnippetRetrieveUpdateDestroyAPIView.as_view()),
 ]
