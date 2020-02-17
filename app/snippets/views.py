a4453671a7930cb7f541514eab563d4cb1e8525d
parkhongbeen <pack122@naver.com>
2020-02-17 16:03:28 +0900

    snippet_detail구현

--- app/snippets/views.py
+++ app/snippets/views.py
@@ -1,10 +1,10 @@
-from django.db.migrations import serializer
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.generics import get_object_or_404
 from rest_framework.parsers import JSONParser
 
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from .models import Snippet
+from .serializers import SnippetSerializer
 
 
 @csrf_exempt
@@ -18,7 +18,26 @@ def snippet_list(request):
         serializer = SnippetSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer, status=201)
+            return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
 
+@csrf_exempt
+def snippet_detail(request, pk):
+    snippet = get_object_or_404(Snippet, pk=pk)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PATCH':
+        data = JSONParser.parse(request)
+        serializer = SnippetSerializer(snippet, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(snippet.data)
+        return JsonResponse(serializer.errors, statue=404)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
